1af72c73f0c925ed8bd20f62b7a296ae06e7e8fd
Victor Hugo Brito Silva <victor.silva4@gpabr.com>
2026-07-02 10:58:12 -0300

    feat: primeiro pipeline apache beam

--- beam/pipeline.py
+++ beam/pipeline.py
@@ -1,11 +1,18 @@
 import apache_beam as beam
-import apache_beam as beam
 
 
 def run():
 
     with beam.Pipeline() as pipeline:
-        pass
+
+        (
+            pipeline
+            | "Ler CSV" >> beam.io.ReadFromText(
+                "database/customers.csv",
+                skip_header_lines=1,
+            )
+            | "Mostrar Linhas" >> beam.Map(print)
+        )
 
 
 if __name__ == "__main__":
